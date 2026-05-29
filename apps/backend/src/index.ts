03f5b0728b53ce19db16c8319542958fb867c4ba
Abhishek <abhi657873@gmail.com>
2026-05-29 12:16:15 +0530

    Use Render port

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -18,7 +18,7 @@ app.get("/", (req, res) => {
   res.send("Backend Runtime API Working");
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
