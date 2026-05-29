856a09ddda36849aae7d909f719c3b13e7130fd8
Abhishek <abhi657873@gmail.com>
2026-05-29 12:56:51 +0530

    Fix Vercel build

--- apps/frontend/src/main.tsx
+++ apps/frontend/src/main.tsx
@@ -4,14 +4,9 @@ import ReactDOM
 from "react-dom/client";
 import App from "./App";
 import "./index.css";
-import {
-  registerSW
-}
-from "virtual:pwa-register";
 
-registerSW({
-  immediate: true,
-});
+
+
 
 ReactDOM.createRoot(
   document.getElementById("root")!
